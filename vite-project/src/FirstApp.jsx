7e710ac1350a1a84512bc124ee86a0d669f4e68d
KuelloKompleteEdition <159363862+KuelloKompleteEdition@users.noreply.github.com>
2024-05-02 01:48:04 -0500

    react 1

---
+++ vite-project/src/FirstApp.jsx
@@ -0,0 +1,15 @@
+const FirstApp = () => {
+    return (
+      <>
+        <h1>
+            Hello World
+        </h1>
+          <span>
+            10
+          </span>
+       
+      </>
+    )
+  }
+
+export default FirstApp
---
+++ vite-project/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import FirstApp from './FirstApp.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FirstApp></FirstApp>
+  </React.StrictMode>,
+)
