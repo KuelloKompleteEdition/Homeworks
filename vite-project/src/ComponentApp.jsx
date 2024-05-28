4abe80c1aaec188de9303ca6d11e5ea4ea91c380
KuelloKompleteEdition <159363862+KuelloKompleteEdition@users.noreply.github.com>
2024-05-28 14:43:32 -0500

    react3

--- vite-project/src/ComponentApp.jsx
+++ vite-project/src/ComponentApp.jsx
@@ -1,16 +1,39 @@
-const categories = ['first category', 'second category']
+import { useState } from "react"
 
-export const ComponentApp = () => {
+ const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['first category']) 
+    const [category, setCategory] = useState('')
+
+    const onAddCategory = () => {
+        // setCategries([...categories, 'hola']) 
+        setCategories( list => [...list, category]) 
+        setCategory('')
+    }
+        
+    const onSetCategory = ( evt ) => {
+        setCategory(evt.target.value)
+    }
+
     return (
         <>
             <h1>GifExpert</h1>
+            <input type="text" value={category}
+                onChange={(event) => onSetCategory(event) } />
+            <button onClick={() => onAddCategory()}> Add Category </button> 
             <ol>
-            {
+                {
                 categories.map(
-                    (category, key) => {
-                        return <li key={ key }> { category } </li>
-                })
-            }
+                    (category, key) =>
+                        {
+                            return <li key={ key }> { category } </li>
+                        }
+                    )
+                }
             </ol>
         </>
-)}
+    )
+}
+
+export default GifExpertApp
+            
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import FirstApp from './FirstApp.jsx'
-import { ComponentApp } from './ComponentApp.jsx'
+import  ComponentApp  from './ComponentApp.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
