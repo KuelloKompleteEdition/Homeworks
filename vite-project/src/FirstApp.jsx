68266b0f4c5ea4b3d3c52e1205e4f9631b23e31a
KuelloKompleteEdition <159363862+KuelloKompleteEdition@users.noreply.github.com>
2024-05-28 18:05:34 -0500

    react5

--- vite-project/src/FirstApp.jsx
+++ vite-project/src/FirstApp.jsx
@@ -1,27 +1,16 @@
 import { useState } from "react"
+import { useCounter } from "./hooks/useCounter"
 
 const FirstApp = ({ value }) => {
-    const [counter, setCounter] = useState( value )
-
-    const handleAdd = () => { 
-        setCounter(counter + 1 )
-    }
-
-    const handleSubstract = () => { 
-        setCounter( counter - 1)
-    }
-
-    const handleReset = () => { 
-        setCounter( value )
-    }
+    const {counter, handleAdd, handleSubstract, handleReset} = useCounter( value )
 
     return (
     <>
         <h1> Counter </h1>
         <span> { counter } </span>
-        <button onClick={() => handleAdd() }> +1 </button>
-        <button onClick={ () => handleSubstract() }> -1 </button> 
-        <button onClick={() => handleReset() }> Reset </button> 
+        <button onClick={handleAdd}> +1 </button>
+        <button onClick={handleSubstract}> -1 </button> 
+        <button onClick={handleReset}> Reset </button> 
     </>
     )}
 
