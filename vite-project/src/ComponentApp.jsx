8d7cb5a63d838bbb0fc5ab50249800e6156df8bc
KuelloKompleteEdition <159363862+KuelloKompleteEdition@users.noreply.github.com>
2024-05-28 14:04:10 -0500

    stash""

---
+++ vite-project/src/ComponentApp.jsx
@@ -0,0 +1,16 @@
+const categories = ['first category', 'second category']
+
+export const ComponentApp = () => {
+    return (
+        <>
+            <h1>GifExpert</h1>
+            <ol>
+            {
+                categories.map(
+                    (category, key) => {
+                        return <li key={ key }> { category } </li>
+                })
+            }
+            </ol>
+        </>
+)}
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import FirstApp from './FirstApp.jsx'
+import { ComponentApp } from './ComponentApp.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <FirstApp value = {10}></FirstApp>
+    <ComponentApp></ComponentApp>
   </React.StrictMode>,
 )
